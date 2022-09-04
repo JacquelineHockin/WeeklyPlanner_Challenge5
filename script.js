ba1eb9f0b3e3b850afb2887c9cb7167abde0de32
Jacqueline Cashman <jacquelinehcashman@gmail.com>
2022-09-03 20:27:33 -1000

    added two for loops to change military time to AM/PM

--- script.js
+++ script.js
@@ -7,12 +7,14 @@
 }
 
 $(document).ready(function () {
-  for (var i = 9; i < 18; i++) {
+  for (var i = 9; i < 12; i++) {
     //<> creates a new element
     var rowDiv = $("<div>")
       .addClass("row time-block")
       .attr("id", "row-" + i);
-    var hourDiv = $("<div>").addClass("col-2 hour").text(i);
+    var hourDiv = $("<div>")
+      .addClass("col-2 hour")
+      .text(i + " AM");
     var textDiv = $("<textarea>").addClass("col-8 description");
     var saveBtn = $("<button>")
       .addClass("col-2 saveBtn")
@@ -28,7 +30,59 @@ $(document).ready(function () {
       rowDiv.append(hourDiv, textDiv, saveBtn.append(icon))
     );
   }
+  //for loop exclusively to make 12AM --> 12PM
+  for (var i = 12; i < 13; i++) {
+    var rowDiv = $("<div>")
+      .addClass("row time-block")
+      .attr("id", "row-" + i);
+    var hourDiv = $("<div>")
+      .addClass("col-2 hour")
+      .text(i + " PM");
+    var textDiv = $("<textarea>").addClass("col-8 description");
+    var saveBtn = $("<button>")
+      .addClass("col-2 saveBtn")
+      .attr("id", "btn-" + i)
+      .click(function () {
+        var num = $(this).attr("id");
+        var hourTxtBox = $(this).siblings("textarea").val();
+        localStorage.setItem("textarea-" + num, hourTxtBox);
+        console.log(hourTxtBox);
+      });
+    var icon = $("<i>").addClass("fas fa-save");
+    $(".container").append(
+      rowDiv.append(hourDiv, textDiv, saveBtn.append(icon))
+    );
+  }
+
+  //for loop for 1-5PM
+  for (var i = 1; i < 6; i++) {
+    var rowDiv = $("<div>")
+      .addClass("row time-block")
+      .attr("id", "row-" + i);
+    var hourDiv = $("<div>")
+      .addClass("col-2 hour")
+      .text(i + " PM");
+    var textDiv = $("<textarea>").addClass("col-8 description");
+    var saveBtn = $("<button>")
+      .addClass("col-2 saveBtn")
+      .attr("id", "btn-" + i)
+      .click(function () {
+        var num = $(this).attr("id");
+        var hourTxtBox = $(this).siblings("textarea").val();
+        localStorage.setItem("textarea-" + num, hourTxtBox);
+        console.log(hourTxtBox);
+      });
+    var icon = $("<i>").addClass("fas fa-save");
+    $(".container").append(
+      rowDiv.append(hourDiv, textDiv, saveBtn.append(icon))
+    );
+  }
+
   for (var i = 9; i < 18; i++) {
     //get the item from local storage, use i to select a particular txt area we want (adding val to txt area)
   }
 });
+
+// $("row-9").html("9 AM");
+
+// document.getElementById("row-9").innerHtml = "9AM";
