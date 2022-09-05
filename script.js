e12f18c2a1faab65d433f6df75d06854daec0cc0
Jacqueline Cashman <jacquelinehcashman@gmail.com>
2022-09-04 20:13:05 -1000

    added class divs to html

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-{
-  /* <div class="row">
-        <div class="col-2"></div>
-        <textarea class="col-8"></textarea>
-        <button class="col-2"></button>
-      </div> */
-}
-
 const currentDay = moment().format("LLLL");
 const clock = moment().format("LTS");
 
@@ -33,7 +25,8 @@ $(document).ready(function () {
   //   let now = moment();
   //   console.log(now.format());
   //<> creates a new element
-  for (var i = 9; i < 12; i++) {
+
+  for (var i = 9; i < 18; i++) {
     //entire row
     var rowDiv = $("<div>")
       .addClass("row time-block")
@@ -41,7 +34,7 @@ $(document).ready(function () {
     //left column w/ hr
     var hourDiv = $("<div>")
       .addClass("col-2 hour")
-      .text(i + " AM");
+      .text(i + ":00");
     //middle text area
     var textDiv = $("<textarea>").addClass("col-8 description");
     //past
@@ -63,52 +56,52 @@ $(document).ready(function () {
     );
   }
   //for loop exclusively to make 12AM --> 12PM
-  for (var i = 12; i < 13; i++) {
-    var rowDiv = $("<div>")
-      .addClass("row time-block")
-      .attr("id", "row-" + i);
-    var hourDiv = $("<div>")
-      .addClass("col-2 hour")
-      .text(i + " PM");
-    var textDiv = $("<textarea>").addClass("col-8 description");
-    var saveBtn = $("<button>")
-      .addClass("col-2 saveBtn")
-      .attr("id", "btn-" + i)
-      .click(function () {
-        var num = $(this).attr("id");
-        var hourTxtBox = $(this).siblings("textarea").val();
-        localStorage.setItem("textarea-" + num, hourTxtBox);
-        console.log(hourTxtBox);
-      });
-    var icon = $("<i>").addClass("fas fa-save");
-    $(".container").append(
-      rowDiv.append(hourDiv, textDiv, saveBtn.append(icon))
-    );
-  }
+  //   for (var i = 12; i < 13; i++) {
+  //     var rowDiv = $("<div>")
+  //       .addClass("row time-block")
+  //       .attr("id", "row-" + i);
+  //     var hourDiv = $("<div>")
+  //       .addClass("col-2 hour")
+  //       .text(i + " PM");
+  //     var textDiv = $("<textarea>").addClass("col-8 description");
+  //     var saveBtn = $("<button>")
+  //       .addClass("col-2 saveBtn")
+  //       .attr("id", "btn-" + i)
+  //       .click(function () {
+  //         var num = $(this).attr("id");
+  //         var hourTxtBox = $(this).siblings("textarea").val();
+  //         localStorage.setItem("textarea-" + num, hourTxtBox);
+  //         console.log(hourTxtBox);
+  //       });
+  //     var icon = $("<i>").addClass("fas fa-save");
+  //     $(".container").append(
+  //       rowDiv.append(hourDiv, textDiv, saveBtn.append(icon))
+  //     );
+  //   }
 
-  //for loop for 1-5PM
-  for (var i = 1; i < 6; i++) {
-    var rowDiv = $("<div>")
-      .addClass("row time-block")
-      .attr("id", "row-" + i);
-    var hourDiv = $("<div>")
-      .addClass("col-2 hour")
-      .text(i + " PM");
-    var textDiv = $("<textarea>").addClass("col-8 description");
-    var saveBtn = $("<button>")
-      .addClass("col-2 saveBtn")
-      .attr("id", "btn-" + i)
-      .click(function () {
-        var num = $(this).attr("id");
-        var hourTxtBox = $(this).siblings("textarea").val();
-        localStorage.setItem("textarea-" + num, hourTxtBox);
-        console.log(hourTxtBox);
-      });
-    var icon = $("<i>").addClass("fas fa-save");
-    $(".container").append(
-      rowDiv.append(hourDiv, textDiv, saveBtn.append(icon))
-    );
-  }
+  //   //for loop for 1-5PM
+  //   for (var i = 1; i < 6; i++) {
+  //     var rowDiv = $("<div>")
+  //       .addClass("row time-block")
+  //       .attr("id", "row-" + i);
+  //     var hourDiv = $("<div>")
+  //       .addClass("col-2 hour")
+  //       .text(i + " PM");
+  //     var textDiv = $("<textarea>").addClass("col-8 description");
+  //     var saveBtn = $("<button>")
+  //       .addClass("col-2 saveBtn")
+  //       .attr("id", "btn-" + i)
+  //       .click(function () {
+  //         var num = $(this).attr("id");
+  //         var hourTxtBox = $(this).siblings("textarea").val();
+  //         localStorage.setItem("textarea-" + num, hourTxtBox);
+  //         console.log(hourTxtBox);
+  //       });
+  //     var icon = $("<i>").addClass("fas fa-save");
+  //     $(".container").append(
+  //       rowDiv.append(hourDiv, textDiv, saveBtn.append(icon))
+  //     );
+  //   }
 
   for (var i = 9; i < 18; i++) {
     //get the item from local storage, use i to select a particular txt area we want (adding val to txt area)
@@ -120,10 +113,10 @@ var currentHour = cal.getHours(); // returns 0-23 on a 24 hour clock
 console.log(currentHour);
 for (var i = 9; i < 18; i++) {
   if (i < currentHour) {
-    document.getElementById("row-" + i).addClass("present");
+    document.getElementById("row-" + i).classList("present");
   } else if (i === currentHour) {
-    document.getElementById("row-" + i).addClass("present");
+    document.getElementById("row-" + i).classList("present");
   } else if (i > currentHour) {
-    document.getElementById("row-" + i).addClass("future");
+    document.getElementById("row-" + i).classList("future");
   }
 }
